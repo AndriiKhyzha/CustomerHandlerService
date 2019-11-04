ddaf1eef68f5069d05e5b9d7de74e5d3edc40e44
YevhenKhyzha <hizhaeugeniy@gmale.com>
2019-11-04 20:25:25 +0100

    1_creation_customer_rest_api, removing unused imports

--- src/main/java/com/programwithAndrii/restservice/RestApp/CustomerRestController.java
+++ src/main/java/com/programwithAndrii/restservice/RestApp/CustomerRestController.java
@@ -1,7 +1,6 @@
 package com.programwithAndrii.restservice.RestApp;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.websocket.server.PathParam;
