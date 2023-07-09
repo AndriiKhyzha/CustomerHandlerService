079912296293513b1c16821d15d029ce3dec98b7
Andrii <andriikhyzha@gmail.com>
2023-07-09 19:18:50 +0200

    1. need to Solve CustomerServiceImpl class with methods inside. 2. connection between IDE and Data Base done successfully. 3. Need to do method getCustomer in CustomerService use able.

--- src/main/java/com/programwithAndrii/restservice/RestApp/CustomerRestController.java
+++ src/main/java/com/programwithAndrii/restservice/RestApp/CustomerRestController.java
@@ -1,7 +1,7 @@
 package com.programwithAndrii.restservice.RestApp;
 
 import com.programwithAndrii.restservice.RestApp.Controllers.Customer;
-import com.programwithAndrii.restservice.RestApp.Model.CustomerService;
+import com.programwithAndrii.restservice.RestApp.Model.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,30 +10,30 @@ import org.springframework.web.bind.annotation.*;
 public class CustomerRestController {
 
     @Autowired
-    private CustomerService customerService;
+    private CustomerServiceImpl customerServiceImpl;
 
-    @GetMapping(value = "/{id}", produces = "application/json")
+    @GetMapping(value = "/{name, id}", produces = "application/json")
     public @ResponseBody
-    Customer getCustomer(String name, @PathVariable("id") String id){
-        Customer customer = new Customer();
-        return customerService.getCustomer("Scuko", "Bliad");
+    Customer getCustomer(@PathVariable ("Robbie") String name, @PathVariable("3016") String id){
+        Customer customer = new Customer(name, id);
+        return customerServiceImpl.getCustomer(name, id);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCustomer(@PathVariable("id") String id) {
-        customerService.deleteCustomer(id);
+        customerServiceImpl.deleteCustomer(id);
     }
 
     @PostMapping(produces = "application/json", consumes = "application/json")
     public @ResponseBody Customer createCustomer(@RequestBody Customer customer) {
-        customerService.createCustomer(customer);
+        customerServiceImpl.createCustomer(customer);
         System.out.println(customer.getId());
         return customer;
     }
 
     @PutMapping(produces = "application/json", consumes = "application/json")
     public @ResponseBody Customer updateCustomer(@RequestBody Customer customer){
-        Customer updatedCustomer = customerService.updateCustomer(customer);
+        Customer updatedCustomer = customerServiceImpl.updateCustomer(customer);
         System.out.println(updatedCustomer);
         return updatedCustomer;
     }
