674ca10444cc155caba33d3901c44c003f09cc04
Andrii <andriikhyzha@gmail.com>
2024-01-07 11:03:43 +0100

    1. Created customer-handler-service.yml for logging. 2. Changed application.yml with created logging settings. 3. Changed log4j to slf4j logger in application. 4. Created RestRequestLogger class for logger configuration settings. 5. Add logging in AddressController & CustomerController classes.

--- customer-handler-service-controller/src/main/java/com/customer/handling/service/controller/CustomerController.java
+++ customer-handler-service-controller/src/main/java/com/customer/handling/service/controller/CustomerController.java
@@ -3,14 +3,14 @@ package com.customer.handling.service.controller;
 import com.customer.handling.service.api.CustomerData;
 import com.customer.handling.service.mapping.ApiControllerModelMapper;
 import com.customer.handling.service.service.CustomerService;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Log4j
+@Slf4j
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
@@ -24,17 +24,20 @@ public class CustomerController {
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CustomerData> getCustomer(@PathVariable("id") Integer id){
         CustomerData customerData = apiControllerModelMapper.map(customerService.getCustomer(id));
+        log.info("get request customer body: " + customerData.dbId() + customerData);
         return ResponseEntity.ok(customerData);
     }
 
     @DeleteMapping("/{id}")
     public void deleteCustomer(@PathVariable("id") Integer id) {
         customerService.deleteCustomer(id);
+        log.info("delete request customer body with Id: " + id);
     }
 
     @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE, consumes = "application/json")
     public @ResponseBody ResponseEntity<CustomerData> createCustomer(@RequestBody CustomerData customerData) {
         CustomerData customerCreateData =  apiControllerModelMapper.map(customerService.createCustomer(apiControllerModelMapper.map(customerData)));
+        log.info("post request customer body with Id: " + customerData.dbId());
         return ResponseEntity.ok(customerCreateData);
     }
 
@@ -43,6 +46,7 @@ public class CustomerController {
     public ResponseEntity<CustomerData> updateCustomer(@RequestBody CustomerData customerData){
         CustomerData customerDataUpdated = apiControllerModelMapper
                 .map(customerService.updateCustomer(apiControllerModelMapper.map(customerData)));
+        log.info("update request customer body with Id: " + customerData.dbId());
         return ResponseEntity.ok(customerDataUpdated);
     }
 }
